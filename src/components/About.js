9e5575cee6d3a3930cafc871c82fc05705d6991d
Hosea <karanja.mungai23@gmail.com>
2024-10-30 15:09:59 +0300

    modifies react components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+
+
+function About ({image = "https://via.placeholder.com/215", about}){
+    return(
+        <aside>
+            <img alt="blog logo" src= {image}/>
+            <p>{about}</p>
+        </aside>
+    )
+}
+
+export default About
---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import Article from "./Article"
+
+function ArticleList (){
+    return(
+        <main>
+            
+         <Article title="Curriculum Vitae" preview="Now that you've learned how to work with components in React, it's time to build something and put those skills to use! Your goal for this lab is to make a static site in React to practice building components, writing JSX, and passing down data as props."/>  
+         <Article title="Curriculum Vitae" preview="Now that you've learned how to work with components in React, it's time to build something and put those skills to use! Your goal for this lab is to make a static site in React to practice building components, writing JSX, and passing down data as props."/>  
+         <Article title="Curriculum Vitae" preview="Now that you've learned how to work with components in React, it's time to build something and put those skills to use! Your goal for this lab is to make a static site in React to practice building components, writing JSX, and passing down data as props."/>  
+            
+        </main>
+    )
+}
+export default ArticleList
